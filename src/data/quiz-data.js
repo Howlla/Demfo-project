32a95762d271df719fd36259c1d67dded8812b8c
Bhavye <bhavye.sharma1996@gmail.com>
2019-08-07 15:42:29 +0530

    first commti

---
+++ src/data/quiz-data.js
@@ -0,0 +1,110 @@
+import React from 'react';
+
+const QUESTION_DATA = [
+  {
+    question: <span>Who is India's first female commercial pilot?</span>,
+    answers: [
+      <span>Durga Banerjee.</span>,
+      <span>Mary Kom.</span>,
+      <span>Mithali Raj.</span>,
+      <span>Amrita Sher-Gil.</span>
+    ],
+    correct: 0
+  },
+  {
+    question: <span>Who is the highest run scorer in women's international cricket?</span>,
+    answers: [
+      <span>Anita Kanwar.</span>,
+      <span>Taniya Bhatia.</span>,
+      <span>Mithali Raj.</span>,
+      <span>Tim Seifert.</span>
+    ],
+    correct: 2
+  },
+  {
+    question: <span>First woman to join Indian Police Services?</span>,
+    answers: [
+      <span>Meera Borwankar.</span>,
+      <span>Sonia Narang.</span>,
+      <span>Kiran Bedi.</span>,
+      <span>Lakshmi Sahgal.</span>
+    ],
+    correct: 2
+  },
+  {
+    question: <span>First female of Indian Origin to go to space?</span>,
+    answers: [
+      <span>Sunita Williams.</span>,
+      <span>Sunita Malhotra.</span>,
+      <span>Ravisha Raina.</span>,
+      <span>Kalpana Chawla.</span>
+    ],
+    correct: 3
+  },
+  {
+    question: <span>First female judge of Indian Supreme Court </span>,
+    answers: [
+      <span>M. Fathima Beevi.</span>,
+      <span>Sheila Dixit.</span>,
+      <span>Uma Bharti.</span>,
+      <span>Mamta Banerjee.</span>
+    ],
+    correct: 0
+  },
+  {
+    question: <span>First female of Indian origin to climb mount everest?</span>,
+    answers: [
+      <span>Mary Kom.</span>,
+      <span>Bachendri Pal.</span>,
+      <span>Aditi Chauhan.</span>,
+      <span>Kalpana Chawla.</span>
+    ],
+    correct: 1
+  },
+  {
+    question: <span>First female doctor in India and the first Indian woman to obtain a medical degree in the United States</span>,
+    answers: [
+      <span>Dr. Rekha Saluja.</span>,
+      <span>Dr. Padmavati.</span>,
+      <span>Dr. Anandi Gopal Joshi.</span>,
+      <span>Dr. Indira Hinduja.</span>
+    ],
+    correct: 2
+  },
+  {
+    question: <span>Women are at increased risk of <strong>Dry Eye </strong>compared to men<br/>
+    <code>what is the risk level?</code>
+    </span>,
+    answers: [
+      <span>55%.</span>,
+      <span>77%.</span>,
+      <span>70%.</span>,
+      <span>90%.</span>
+    ],
+    correct: 2
+  },
+  {
+    question: <span>As per <strong>DEWS ll</strong> women are found to use more artificial tears as compared to men<br/>
+    <code>How much more do you think?</code></span>,
+    answers: [
+      <span>15%.</span>,
+      <span>20%.</span>,
+      <span>25%.</span>,
+      <span>35%.</span>
+    ],
+    correct: 1
+  },
+  {
+    question: <span>Studies have shown women are more likely to present severe dry eyes<br/>
+      <code>What percentage of women experience it</code> </span>,
+    answers: [
+      <span>12%.</span>,
+      <span>25%.</span>,
+      <span>30%.</span>,
+      <span>33%.</span>
+    ],
+    correct: 3
+  }
+];
+
+export default QUESTION_DATA;
